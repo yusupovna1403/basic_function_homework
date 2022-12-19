a83a6ea4845e544777880de3677e73ded6c4c5dc
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-19 08:22:00 +0500

    changed basic_func02

--- basic_func02.py
+++ basic_func02.py
@@ -1,7 +1,7 @@
 # Create a function called main.
 
 # Return the value "Hello World"
-def main(name):
-    return name
-x = main("Hello word")
-print(x)
+def main():
+    name = "Hello world"
+    return name 
+print(main())
